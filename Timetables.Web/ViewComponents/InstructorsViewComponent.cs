e569ffa90def9cec6255faa035c181d8a7bee18a
activedan <daniel.ward.se@gmail.com>
2022-02-11 19:02:09 +1100

    Adding example for a ViewComponent

---
+++ Timetables.Web/ViewComponents/InstructorsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Timetables.Web.Engine.Services;
+using Timetables.Web.Models.Instructors;
+
+namespace Timetables.Web.ViewComponents
+{
+    [ViewComponent(Name = "Instructors")]
+    public class InstructorsViewComponent : ViewComponent
+    {
+        private readonly IFestivalsService _festivalsService;
+
+        public InstructorsViewComponent(IFestivalsService festivalsService)
+        {
+            _festivalsService = festivalsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid festivalId)
+        {
+            var festival = _festivalsService.GetFestival(festivalId);
+
+            var viewModel = new InstructorsViewModel(festival);
+
+            return View("_Instructors", viewModel);
+        }
+    }
+}
