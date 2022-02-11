9aaf6d79d05b58e12e6b99167c9c5d00e26fac45
activedan <daniel.ward.se@gmail.com>
2022-02-11 19:24:37 +1100

    Added partial view example

--- Timetables.Web/Models/FestivalViewModel.cs
+++ Timetables.Web/Models/FestivalViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Timetables.Web.Engine.Models;
+using Timetables.Web.Models.Instructors;
 
 namespace Timetables.Web.Models
 {
@@ -8,10 +9,14 @@ namespace Timetables.Web.Models
         public Guid FestivalId { get; set; }
         public string FestivalName { get; set; }
 
+        public InstructorsViewModel Instructors { get; set; }
+
         public FestivalViewModel(Festival festival)
         {
             FestivalId = festival.FestivalId;
             FestivalName = festival.Name;
+
+            Instructors = new InstructorsViewModel(festival);
         }
     }
 }
